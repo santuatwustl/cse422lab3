3b5b0fa022c2f7f566999a67c31557f49c6e778f
Yaowen Wang <wang.yaowen@wustl.edu>
2018-12-06 00:04:04 -0600

    3

--- mod/.paging.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/santu/Desktop/lab-3/mod/paging.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/santu/Desktop/lab-3/mod/paging.ko /home/santu/Desktop/lab-3/mod/paging.o /home/santu/Desktop/lab-3/mod/paging.mod.o ;  true
--- mod/.tmp_versions/paging.mod
+++
@@ -1,3 +0,0 @@
-/home/santu/Desktop/lab-3/mod/paging.ko
-/home/santu/Desktop/lab-3/mod/paging.o
-
