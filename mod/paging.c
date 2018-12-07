f3ac166e20ca69be463372c1c60ec4331c7cf4df
yaowen wang <wang.yaowen@wustl.edu>
2018-12-07 10:40:51 -0600

    1

--- mod/paging.c
+++ mod/paging.c
@@ -9,23 +9,32 @@
 #include <linux/slab.h>
 #include <linux/memory.h>
 #include <linux/mm.h>
+#include <linux/types.h>
 
 #include "paging.h"
 
+struct vma_data{
+    atomic_t counter;
+    struct page * page_address;
+} this_one;
 
+atomic_t page_creation_counter = ATOMIC_INIT(0);
+atomic_t page_free_counter = ATOMIC_INIT(0);
 static int
 do_fault(struct vm_area_struct * vma,
          unsigned long           fault_address)
 {
+    printk("do_fault, vma: %lu\n", vma);
 
     struct page *new_page = alloc_page(GFP_KERNEL);
+    atomic_inc(&page_creation_counter);
     if(!new_page){
         return VM_FAULT_OOM;
     } 
     unsigned long page_number = page_to_pfn(new_page);
     unsigned long aligned_vaddress = PAGE_ALIGN(fault_address);
     int remaped = remap_pfn_range(vma, aligned_vaddress, page_number, PAGE_SIZE, vma->vm_page_prot);
-    printk("page number: %lu\n remapped number: %d\n aligned_vaddress: %lu\n", page_number, remaped, aligned_vaddress);
+    // printk("page number: %lu\n remapped number: %d\n aligned_vaddress: %lu\n", page_number, remaped, aligned_vaddress);
     if(!remaped) return VM_FAULT_NOPAGE;
     printk(KERN_INFO "paging_vma_fault() invoked: took a page fault at VA 0x%lx\n", fault_address);
     return VM_FAULT_SIGBUS;
@@ -45,7 +54,7 @@ paging_vma_fault(struct vm_fault * vmf)
     struct vm_area_struct * vma = vmf->vma;
     unsigned long fault_address = (unsigned long)vmf->address;
 #endif
-
+    printk("paging_vma_fault, vmf: %lu\n", vmf->vma);
     return do_fault(vma, fault_address);
 }
 
@@ -58,6 +67,9 @@ paging_vma_open(struct vm_area_struct * vma)
 static void
 paging_vma_close(struct vm_area_struct * vma)
 {
+    // __page_free();
+    atomic_inc(&page_free_counter);
+    printk("paging_vma_close, vma: %lu\n", vma);
     printk(KERN_INFO "paging_vma_close() invoked\n");
 }
 
@@ -82,6 +94,7 @@ paging_mmap(struct file           * filp,
 
     /* setup the vma->vm_ops, so we can catch page faults */
     vma->vm_ops = &paging_vma_ops;
+    printk("paging_mmap, vma: %lu", vma);
 
     printk(KERN_INFO "paging_mmap() invoked: new VMA for pid %d from VA 0x%lx to 0x%lx\n",
         current->pid, vma->vm_start, vma->vm_end);
@@ -108,8 +121,13 @@ dev_handle =
 static int
 kmod_paging_init(void)
 {
-    int status;
 
+    struct vma_data *this_vma_data;
+    this_vma_data = kmalloc(sizeof(this_one), GFP_KERNEL);
+    atomic_set(&(this_vma_data->counter), 0);
+    printk("%d \n", atomic_read(&(this_vma_data->counter)));
+
+    int status;
     /* Create a character device to communicate with user-space via file I/O operations */
     status = misc_register(&dev_handle);
     if (status != 0) {
